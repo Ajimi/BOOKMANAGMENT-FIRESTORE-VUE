975aa088d14ee368846ac53a271c30d9a562d32d
ajimi <sloumma.21@gmail.com>
2017-12-17 09:59:11 +0100

    Firebase configuration

---
+++ src/components/config.js
@@ -0,0 +1,9 @@
+export default {
+  apiKey: '',
+  authDomain: 'bookmanagment-320e5.firebaseapp.com',
+  databaseURL: 'https://bookmanagment-320e5.firebaseio.com',
+  projectId: 'bookmanagment-320e5',
+  storageBucket: 'bookmanagment-320e5.appspot.com',
+  messagingSenderId: '309979509176'
+}
+
