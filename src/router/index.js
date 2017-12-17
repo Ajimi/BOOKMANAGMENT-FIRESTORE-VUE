ded011168d8a88a1cbcbb0c95a1e70fdb254b741
ajimi <sloumma.21@gmail.com>
2017-12-17 09:59:59 +0100

    Update router configuration

--- src/router/index.js
+++ src/router/index.js
@@ -1,25 +1,38 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Vuetify from 'vuetify'
+import VeeValidate from 'vee-validate'
 import Navbar from '@/components/Navbar'
 import BookAdd from '@/components/BookAdd'
 import BookView from '@/components/BookView'
+import BookList from '@/components/BookList'
 import BookEdit from '@/components/BookEdit'
 
 Vue.use(Router)
 Vue.use(Vuetify)
+Vue.use(VeeValidate)
 
 export default new Router({
   routes: [
     {
       path: '/',
-      name: 'BookView',
-      component: BookView
+      name: 'BookList',
+      component: BookList
     },
     {
       path: '/new',
       name: 'BookAdd',
       component: BookAdd
+    },
+    {
+      path: '/edit/:book_id',
+      name: 'BookEdit',
+      component: BookEdit
+    },
+    {
+      path: '/:book_id',
+      name: 'BookView',
+      component: BookView
     }
   ]
 })
